3c2389613d40df22812a9b137f4fbb1bff1ad26f
miltonmrq <miltonmarquez@iresm.edu.ar>
2022-06-08 09:53:31 -0300

    trabajo

---
+++ practica.js
@@ -0,0 +1,68 @@
+/* 1) 
+estudiantes = ["Nelson", "Sofia", "Salma", "Julieta", "Silena"]
+document.write(estudiantes);
+*/
+
+
+/* 2) 
+console.log(estudiantes = [4]) 
+document.write(estudiantes);
+*/
+
+/* 3)
+estudiantes [2] = "Morena"
+document.write(estudiantes);
+*/
+
+
+/* 4)
+estudiantes.push("Alberto")
+alert(estudiantes);
+*/
+
+/* 5)
+estudiantes.pop();
+document.write(estudiantes);
+*/
+
+/* 6)
+estudiantes.unshift("Morena") 
+document.write(estudiantes);
+*/
+
+/* 7)
+estudiantes.shift() 
+document.write(estudiantes);
+*/
+
+
+/* 8)
+ingresantes2023 = ["Jose", "Franco", "Tobias"]
+nuevoAño = estudiantes.concat(ingresantes2023); 
+document.write(nuevoAño);
+*/
+
+/* 9)
+nuevoAño.sort() 
+document.write(nuevoAño);
+*/
+
+
+/* 10)
+let salir = 'n'
+let lista_de_articulos = []
+
+while (salir == 'n') {
+    lista = prompt('ingresa un articulo')
+    if (lista != null) {
+        lista_de_articulos.push(lista)
+    }
+    let seguir = confirm('desea ingresar mas articulos?')
+    if (!seguir) {
+        salir = 'y'
+    }
+}
+console.log(lista_de_articulos) 
+*/
+
+
